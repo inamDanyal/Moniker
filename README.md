e71181c85a911779994b4013e700667d69ab3b49
inamDanyal <inamdanyal@gmail.com>
2023-08-18 23:25:00 +0100

    updated frontend

---
+++ README.md
@@ -0,0 +1,5 @@
+#Moniker
+
+A web app that will allow you to download manga for offline viewing
+
+Inspired by Hakuneko
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section id="one">
+        <div>
+          <h2>Search Manga</h2>
+          <input type="text" id="input"></input>
+        </div>
+      </section>
     </div>
   );
 }
